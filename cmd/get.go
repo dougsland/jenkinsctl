75d43102c53ee31be299eb96f776a0eb8d9f7514
Douglas Schilling Landgraf <dougsland@redhat.com>
2021-06-12 19:40:38 -0400

    update

--- cmd/get.go
+++ cmd/get.go
@@ -139,7 +139,7 @@ var nodesOnline = &cobra.Command{
 	Short: "get nodes online",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("⏳ Collecting node(s) information...\n")
-		err := jenkinsMod.ShowNodes("online")
+		_, err := jenkinsMod.ShowNodes("online")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/dougsland/jenkinsctl
 go 1.16
 
 require (
-	github.com/dougsland/jenkinsctl/jenkins v0.0.0-20210612205919-36f7a92d73cf
+	github.com/dougsland/jenkinsctl/jenkins v0.0.0-20210612233900-c06f56333629
 	github.com/spf13/cobra v1.1.3
 )
